7b97934a1a42005d0100cbfe8564bc7edbe0be62
SerhiyBogachenko <55495768+SerhiyBogachenko@users.noreply.github.com>
2022-07-14 22:43:27 +0300

    HW_3 (14.07.2022)

---
+++ Home_Work/HW_3.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+# Assignment 02-01
+#
+# Task: Implement cylinder volume formula:
+#
+# Formula: V=pi*r*r*h, where
+#
+#          V - volume of cylinder
+#          pi - pi math constant
+#          r - radius of the circular base
+#          h - height of cylinder
+
+pi = 3.14
+r = 5
+h = 10
+V = pi * r * r * h
+
+print(f"Volume of cylinder with radius r={r} and height h={h}: {V}")
