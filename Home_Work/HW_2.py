61f5b110803244a319d7836cff7ffe386b1fb46d
SerhiyBogachenko <55495768+SerhiyBogachenko@users.noreply.github.com>
2022-07-14 22:34:00 +0300

    HW_2 (14.07.2022)

---
+++ Home_Work/HW_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# Assignment 01-01
+#
+# Assign actual data to the variables below
+# and run the script to print the output.
+
+name = "Serhii"
+profession = "Manual QA engineer"
+facility = "Nayax"
+city = "Odessa"
+country = "Ukraine"
+goal = "AQA engineer"
+age = 30
+
+print(
+f"""
+My name is {name}. I am a {profession} at {facility} 
+in {city}, {country}. I want to learn how to program
+in Python to become a {goal}. I am {age} years old.
+"""
+) 
